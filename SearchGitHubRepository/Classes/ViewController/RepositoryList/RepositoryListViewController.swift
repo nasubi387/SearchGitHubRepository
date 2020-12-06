146f9163467d801040b99dec552c75bdf9a3295c
Naoya Ishida <>
2020-12-06 14:05:34 +0900

    add error handling and indicator

--- SearchGitHubRepository/Classes/ViewController/RepositoryList/RepositoryListViewController.swift
+++ SearchGitHubRepository/Classes/ViewController/RepositoryList/RepositoryListViewController.swift
@@ -9,10 +9,27 @@ import UIKit
 
 class RepositoryListViewController: UIViewController {
     
+    /// 取得したレポジトリのリスト。値を更新するとtableViewをリロードする。
     var repositories = [Repository]() {
         didSet {
             DispatchQueue.main.async { [weak self] in
                 self?.tableView.reloadData()
+            }
+        }
+    }
+    
+    /// 通信中フラグ。通信状態によって各UIの表示状態を制御する。また、通信中はUI操作ができないようにsuper viewのユーザインタラクションフラグをfasleにする
+    var isLoading: Bool = false {
+        didSet {
+            // 常にUI操作はメインスレッドで実行する
+            DispatchQueue.main.async { [weak self] in
+                guard let self = self else {
+                    return
+                }
+                self.tableView.isHidden = self.isLoading
+                self.indicator.isHidden = !self.isLoading
+                self.isLoading ? self.indicator.startAnimating() : self.indicator.stopAnimating()
+                self.view.isUserInteractionEnabled = !self.isLoading
             }
         }
     }
@@ -32,13 +49,22 @@ class RepositoryListViewController: UIViewController {
             searchBar.placeholder = "検索"
         }
     }
+    @IBOutlet weak var indicator: UIActivityIndicatorView! {
+        didSet {
+            indicator.style = .large
+            indicator.isHidden = true
+        }
+    }
     
     // MARK:- LifeCycle
     
+    /// 画面読込時処理
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
+    /// 画面表示直前処理
+    /// - Parameter animated: アニメーション有無
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
@@ -48,13 +74,38 @@ class RepositoryListViewController: UIViewController {
     
     // MARK:- Action
     
+    /// 画面タイトルを更新する
+    /// - Parameter title: タイトル
     func updateTitle(with title: String) {
-        navigationItem.title = title
+        DispatchQueue.main.async { [weak self] in
+            self?.navigationItem.title = title
+        }
+    }
+    
+    /// エラーアラートを表示する
+    /// - Parameter message: エラーメッセージ
+    func presentError(message: String) {
+        DispatchQueue.main.async { [weak self] in
+            let alert = UIAlertController(title: nil, message: message, preferredStyle: .alert)
+            let okButton = UIAlertAction(title: "OK", style: .default, handler: nil)
+            alert.addAction(okButton)
+            self?.present(alert, animated: true, completion: nil)
+            self?.updateTitle(with: "Search GitHub Repository")
+        }
     }
     
     // MARK:- API
     
+    /// 検索API実行
+    /// - Parameter keyword: 検索キーワード
     func fetchRepository(with keyword: String) {
+        // 通信中の場合は処理を実行しない
+        guard !isLoading else {
+            return
+        }
+        isLoading = true
+        // 前回の検索結果をクリア
+        repositories = []
         // セッション作成
         let config = URLSessionConfiguration.default
         let session = URLSession(configuration: config)
@@ -72,18 +123,29 @@ class RepositoryListViewController: UIViewController {
                 return
             }
             
+            // 通信処理完了後に通信フラグを戻す
+            defer {
+                self.isLoading = false
+            }
+            
+            // エラーが発生した場合はエラー表示
             guard let data = data, error == nil else {
-                // ToDo: Error Handling
-                print(error?.localizedDescription ?? "")
+                if let message = error?.localizedDescription {
+                    self.presentError(message: message)
+                }
                 return
             }
             
             let decoder = JSONDecoder()
             do {
+                // レスポンスのJSONデータをレスポンス構造体にデコードし、表示するデータとしてrepositoriesに保持する
                 let response = try decoder.decode(SearchRepositoryResponse.self, from: data)
                 self.repositories = response.items
+                // 検索キーワードを画面タイトルとする
+                self.updateTitle(with: keyword)
             } catch let error {
-                print(error)
+                // デコードに失敗した場合はエラー表示（基本的にここには入らない想定。入った場合は不具合）
+                self.presentError(message: error.localizedDescription)
             }
         }
         // API実行
@@ -92,10 +154,20 @@ class RepositoryListViewController: UIViewController {
 }
 
 extension RepositoryListViewController: UITableViewDataSource {
+    /// 表示セル数。取得したレポジトリの数分リストで表示する
+    /// - Parameters:
+    ///   - tableView: tableView
+    ///   - section: section
+    /// - Returns: セル数
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return repositories.count
     }
     
+    /// 表示セル設定。レポジトリの値でセルの内容を更新する
+    /// - Parameters:
+    ///   - tableView: tableView
+    ///   - indexPath: indexPath
+    /// - Returns: 設定後のセル
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: RepositoryListCell.className, for: indexPath)
         
@@ -108,14 +180,21 @@ extension RepositoryListViewController: UITableViewDataSource {
 }
 
 extension RepositoryListViewController: UITableViewDelegate {
+    /// セルタップ時処理。
+    /// - Parameters:
+    ///   - tableView: tableView
+    ///   - indexPath: indexPath
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
     }
 }
 
 extension RepositoryListViewController: UISearchBarDelegate {
+    /// 検索タップ時処理。キーワードがない場合エラー表示。正常な場合はAPIを実行する
+    /// - Parameter searchBar: searchBar
     func searchBarSearchButtonClicked(_ searchBar: UISearchBar) {
-        guard let keyword = searchBar.text else {
+        guard let keyword = searchBar.text, !keyword.isEmpty else {
+            presentError(message: "検索するキーワードを入力してください。")
             return
         }
         fetchRepository(with: keyword)
